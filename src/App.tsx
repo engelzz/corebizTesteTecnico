df9261ca45bfd17d87047f693a663ed759614633
Matheus Engel <102177203+engelzz@users.noreply.github.com>
2025-01-13 22:23:38 -0300

    feat: setup app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Router } from './app/router/router';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Router />
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <Router />
+      </NavigationContainer>
+    </QueryClientProvider>
   )
 }
