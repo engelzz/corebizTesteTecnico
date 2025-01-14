445fbfa433983b6b87e5a57de54ca30e36a318bd
Matheus Engel <102177203+engelzz@users.noreply.github.com>
2025-01-14 16:36:24 -0300

    feat: Main Screen controller

--- src/ui/MainScreen/useMainScreenController.ts
+++ src/ui/MainScreen/useMainScreenController.ts
@@ -1,77 +1,50 @@
-import {
-  useNavigation
-} from '@react-navigation/native';
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { useState } from "react";
-import { z } from 'zod';
-import { getImages } from '../../app/services/GetImagesService/getImageService';
-import { searchImages } from "../../app/services/SearchImagesService/searchImagesService";
+import { HttpClient } from "../../app/config/httpClient";
+import { ImageObject } from "../../app/entities/ImageResponse";
+import { ImagesService } from "../../app/services/ImagesService/imagesService";
 
-const PhotosProps = z.object({
-  id: z.string(),
-  urls: z.object({
-    small: z.string()
-  }),
-  user: z.object({
-    name: z.string(),
-    bio: z.string(),
-    portifolio_url: z.string(),
-    location: z.string()
-  }),
-  description: z.string().optional(),
-  alt_description: z.string().optional(),
-  asset_type: z.string(),
-  likes: z.number()
-})
+export function useMainScreenController() {
+	const imageService = new ImagesService(new HttpClient());
+	const [query, setQuery] = useState<string>("");
+	const [photos, setPhotos] = useState<ImageObject[]>([]);
 
-type FormData = z.infer<typeof PhotosProps>;
+	const { isFetching, fetchNextPage, refetch } = useInfiniteQuery({
+		queryKey: ["photos"],
+		staleTime: 600000, // 10 minutos para que as imagens não sofram alterações
+		queryFn: async ({ pageParam = 1 }) => {
+			if (query) {
+				const data = await imageService.fetch({
+					queryParams: {
+						query,
+						page: pageParam,
+					},
+				})
+				setPhotos((prevState) => [...prevState, ...data]);
+			} else {
+				const data = await imageService.fetchRandomPhotos();
+				if(data != null){
+					setPhotos(data);
+				}
+			}
 
+			return pageParam;
+		},
+		getNextPageParam: (lastPage) => {
+			return lastPage + 1;
+		},
+	});
 
-export function useMainScreenController () {
-    const [query, setQuery] = useState<string>('');
-    const [photos, setPhotos] = useState<FormData[]>([]);
-    const [loading, setLoading] = useState(false);
-    const [page, setPage] = useState(1);
+	const resetPhotos = () => {
+		setPhotos([]);
+	};
 
-    const navigation = useNavigation();
-
-    const { isFetching } = useQuery({
-      queryKey: ['photos'],
-      queryFn: async () => {
-        const data = await getImages();
-        setPhotos(data);
-        return data;
-      },
-    })
-  
-    const handleSearch = async () => {
-      setLoading(true);
-      const results = await searchImages(query, page);
-      setPhotos(results);
-      setLoading(false);
-    };
-
-    const handleRefecth = async () => {
-      const nextPage = page + 1;
-      setLoading(true);
-
-      const data = await searchImages(query, nextPage);
-
-      if (data) {
-        setPhotos((prevPhotos) => [...prevPhotos, ...data.results]);
-        setPage(nextPage);
-      }
-
-      setLoading(false);
-    };
-  
-    return {
-        query,
-        setQuery,
-        photos,
-        isFetching,
-        handleSearch,
-        navigation,
-        handleRefecth
-    }
+	return {
+		query,
+		setQuery,
+		photos,
+		fetchNextPage,
+		refetch,
+		resetPhotos,
+	};
 }
