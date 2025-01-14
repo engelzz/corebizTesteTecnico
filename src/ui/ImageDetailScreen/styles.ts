a699bb8314a19eeaad86198b7ae09aa2e8d2e69f
Matheus Engel <102177203+engelzz@users.noreply.github.com>
2025-01-14 16:35:58 -0300

    feat: Main and Details Screen styles

---
+++ src/ui/ImageDetailScreen/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.ScrollView`
+		flex: 1;
+		padding-top: 80;
+		background-color: '#F1F3F5';
+`;
+
+export const PhotoTitle = styled.Text`
+	margin-top: 10;
+	font-size: 18;
+	font-weight: 600;
+	color: #111;
+`;
---
+++ src/ui/MainScreen/styles.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+	flex: 1;
+	padding-top: 80;
+	align-items: center;
+	background-color: '#F1F3F5';
+`;
+
+export const Title = styled.Text`
+	font-size: 18;
+	font-weight: bold;
+	margin-top: 10;
+`;
+
+export const AuthorName = styled.Text`
+	font-size: 12;
+	color: '#222';
+	margin-top: 5;
+	font-weight: 800;
+`
+
+export const PhotoTitle = styled.Text`
+	margin-top: 10;
+	font-size: 18;
+	font-weight: 600;
+	color: #111;
+`;
